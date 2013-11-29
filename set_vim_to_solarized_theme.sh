ba80eaf2f7f7151d33a1c0844b9b8b8df7626be3
Will Yang <yang.xiaowei@gmail.com>
2013-11-29 15:51:25 +0800

    added script to install solarized theme

---
+++ set_vim_to_solarized_theme.sh
@@ -0,0 +1,18 @@
+# install Pathogen (https://github.com/tpope/vim-pathogen)
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+echo "\n\" Use pathogen.vim\nexecute pathogen#infect()\n" >> ~/.vimrc
+
+# Install vim-colors-solarized (Pathogen installation)
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
+echo "\nsyntax enable\nset background=dark\nset t_Co=16\nlet g:solarized_termcolors=16\ncolorscheme solarized\n" >> ~/.vimrc
+
+# For Linux only: Solarized colors on Gnome Terminal
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+./gnome-terminal-colors-solarized/set_dark.sh
+#./gnome-terminal-colors-solarized/set_light.sh
+rm -r gnome-terminal-colors-solarized
